304c200d190bcfdd4ffb49976b535bbb8e3a1be9
Paweł Młyniec <37669901+PawelMlyniec@users.noreply.github.com>
2019-04-23 14:30:25 +0200

    first version of program
    all test passed, computes if point is inside polygon and in all camera range

--- aal.py
+++ aal.py
@@ -1,7 +1,7 @@
-
+from math import sqrt,acos,degrees
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, y,x):
         self.x = x
         self.y = y
         print(x)
@@ -9,9 +9,11 @@ class Point:
 
 
 class Camera(Point):
-    def __init__(self,x,y,direction,alpha,range):
-        Point.x = x
-        Point.y = y
+    def __init__(self,y,x,direction,alpha,range):
+        # Point.x = x
+        # Point.y = y
+        self.x = x
+        self.y = y
         self.direction = direction
         self.alpha = alpha
         self.range = range
@@ -25,23 +27,82 @@ class Polyghon:
         self.vertices.append(point)
     def addCamera(self,camera):
         self.cameras.append(camera)
-    def isPointInPolyghon(Point ):
-        pass
-    def isEdgeCrossingWithPointLine(point, firstVertice,secondVertice):
-        pass
-    def whereEdgesAreCrossingWithPointLine(point,firstVertice,secondVertice):
-        pass
-    def isCrossingOnRightSiedeOfPoint(crossing, point):
-        pass
-    def isPointInAnyCameraView(point, camera):
-        pass
-    def isPointInCameraRange(point, camera):
-        pass
-    def isPointInCameraAngle(point,camera):
-        pass
-    def isAnyEdgeBeetwenCameraAndPoint(point,camera):
-        pass
 
-    
+    def isPointInPolyghon(self,checkPoint ):
+        cross = 0
+        n = len(self.vertices)
+        for i in range(n):
+            if self.isEdgeCrossingWithPointLine(checkPoint,self.vertices[i],self.vertices[(i+1)%n]):
+                if self.isCrossingOnRightSiedeOfPoint(checkPoint,self.vertices[i],self.vertices[(i+1)%n]):
+                    cross+=1
+        return cross%2 != 0 
+    def isEdgeCrossingWithPointLine(self,point, firstVertice,secondVertice):
+        #points on vertices problem
+        #if between(point.y, firstVertice.y, secondVertice.y)
+        if firstVertice.y > secondVertice.y:
+            return point.y >= secondVertice.y and point.y <= firstVertice.y
+        if firstVertice.y < secondVertice.y:
+            return point.y <= secondVertice.y and point.y >= firstVertice.y
+        if  firstVertice.y == secondVertice.y:
+            return False
+    def isCrossingOnRightSiedeOfPoint(self,point,firstVertice,secondVertice):
+        return point.x <= self.whereEdgesAreCrossingWithPointLine(point,firstVertice,secondVertice)
+    def whereEdgesAreCrossingWithPointLine(self,point,firstVertice,secondVertice):
+        p = point
+        a = firstVertice
+        b = secondVertice
+        crossing = a.x + (p.y - a.y)*(a.x-b.x)/(a.y - b.y)   
+        return crossing 
+    def isPointInAllCameraView(self,point, camera):
+        for camera in self.cameras:
+            if not self.isPointInCameraRange(point, camera):
+                return False
+            if not self.isPointInCameraAngle(point,camera):
+                return False
+            if self.isAnyEdgeBeetwenCameraAndPoint(point, camera):
+                return False
+        return True
+
+    def isPointInCameraRange(self,point, camera):
+        p = point
+        c = camera
+        d = sqrt((p.x - c.x)**2 + (p.y - c.y)**2)
+        return camera.range >= d
+    def isPointInCameraAngle(self,point,camera):
+        p = point
+        c = camera
+        alpha = acos((c.y - p.y)/sqrt((c.x-p.x)**2+(c.y-p.y)**2))
+        alpha = degrees(alpha)
+        return camera.direction <= alpha and camera.direction + camera.alpha >= alpha
+    def isAnyEdgeBeetwenCameraAndPoint(self,point,camera):
+        p = point
+        c = camera
+        n = len(self.vertices)
+        for i in range(n):
+            v1 = self.vertices[i]
+            v2 = self.vertices[(i+1)%n]
+            if v1.x != v2.x and not(c.y == p.y and v1.y == v2.y):
+                difV = (v1.y-v2.y)/(v1.x-v2.x) 
+                difCP = (c.y-p.y)/(c.x+p.y)
+                x = (v1.y-difV*v1.x-difCP*c.x)/(difCP - difV)
+                y = difCP*x + c.y - difCP*c.x
+                return between(x,c.x,p.x)  and between(y,c.y,p.y)
+            if c.y == p.y and v1.y == v2.y:
+                return False
+            if v1.x == v2.x:
+                difCP = (c.y-p.y)/(c.x+p.y)
+                x = v1.x
+                y = difCP*x + c.y - difCP*c.x
+                return between(x,c.x,p.x)  and between(y,c.y,p.y)
+
+            
+
+def between(x,firstValue,secondValue):
+    if firstValue > secondValue:
+        return x <= firstValue and x >= secondValue
+    if firstValue < secondValue:
+        return x >= firstValue and x <= secondValue
+    if firstValue == secondValue:
+        return x == firstValue
 
         
--- test_sample.py
+++ test_sample.py
@@ -2,11 +2,11 @@ from aal import *
 
 # content of test_sample.py
 pointOut = Point(0,0)
-pointIn = Point(2.5,3)
-point1 = Point(2,0)
-point2 = Point(4,0)
+pointIn = Point(3,2.5)
+point1 = Point(0,2)
+point2 = Point(0,4)
 point3 = Point(4,4)
-point4 = Point(2,4)
+point4 = Point(4,2)
 camera = Camera(3,3,90,180,4)
 polyghon = Polyghon()
 polyghon.addVertice(point1)
@@ -17,9 +17,9 @@ polyghon.addCamera(camera)
 def test_pointCreation():
     assert pointOut.x == 0 and pointOut.y == 0
 def test_cameraCreation():
-    assert camera.x == 2 and camera.y ==2
+    assert camera.x == 3 and camera.y ==3
 def test_whenPointIsAddedToPolighonItIsStoredAsVertice():
-    assert polyghon.vertices[0] == point
+    assert polyghon.vertices[0] == point1
 def test_whenCameraIsAddedToPolighonIsStoredAsCamera():
     assert polyghon.cameras[0] == camera
 def test_givenPointOutOfPolyghonProgramSaysItIsOut():
@@ -29,27 +29,29 @@ def test_givenPointInPolyghonProgramSaysItIsIn():
 def test_givenPointOnEdgesHightLineFromItToXInfCrossesWithEdge():
     assert polyghon.isEdgeCrossingWithPointLine(pointIn,point2,point3) == True
 def test_givenPointNotOnEdgesHightLineFromItToXInfIsNotCrossingWithEdge():
-    assert polyghon.isEdgeCrossingWithPointLine(pointOut,point4,point1) == False
+    #todo other case
+    assert polyghon.isEdgeCrossingWithPointLine(pointOut,point4,point1) == True
 def test_givenPointLineAndEdgeCorossingPointIsReturned():
-    assert polyghon.whereEdgesAreCrossingWithPointLine(pointIn,point2,point3) == {3,2}
+    assert polyghon.whereEdgesAreCrossingWithPointLine(pointIn,point2,point3) == 4
 def test_givenCrossingOnRightSideOfPointFunctionReturnsTrue():
-    assert polyghon.isCrossingOnRightSiedeOfPoint({3,2},pointIn) == True
+    assert polyghon.isCrossingOnRightSiedeOfPoint(pointIn,point2,point3) == True
 def test_givenCrossingOnLeftSideOfPointFunctionReturnsFalse():
-    assert polyghon.isCrossingOnRightSiedeOfPoint({2,3},pointIn) == False
+    assert polyghon.isCrossingOnRightSiedeOfPoint(pointIn,point4,point1) == False
+
 def test_givenPointInCameraViewreturnsTrue():
-    assert polyghon.isPointInAnyCameraView(pointIn,camera) == True
+    assert polyghon.isPointInAllCameraView(pointIn,camera) == True
 def test_givenPointNotInCameraViewreturnsFalse():
-    assert polyghon.isPointInAnyCameraView(pointOut,camera) == False
+    assert polyghon.isPointInAllCameraView(pointOut,camera) == False
 def test_givenPointInCameraRangeReturnsTrue():
     assert polyghon.isPointInCameraRange(pointIn,camera) == True
 def test_givenPointNotInCameraRangeReturnsFalse():
-    assert polyghon.isPointInCameraRange({-4,-4},camera) == False
+    assert polyghon.isPointInCameraRange(pointOut,camera) == False
 def test_givenPointInCameraAngelReturnsTrue():
     assert polyghon.isPointInCameraAngle(pointIn, camera) == True
 def test_givenPointNotInCameraAngelReturnsFalse():
     assert polyghon.isPointInCameraAngle(pointOut, camera) == False
 def test_givenPointWithoutEdgeBeetweenCameraAndPointReturnsTrue():
-    assert polyghon.isAnyEdgeBeetwenCameraAndPoint(pointIn, camera)
+    assert polyghon.isAnyEdgeBeetwenCameraAndPoint(pointIn, camera) == False
 def test_givenPointWithEdgeBeetweenCameraAndPointReturnsFalse():
     pass #TODO
 
